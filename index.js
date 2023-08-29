de8453144b87ebca6a00a4e2a549084542031391
woodley <gelinw3@gmail.com>
2023-08-29 01:45:34 -0400

    try 11

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(
     proxy: true,
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none"
     },
