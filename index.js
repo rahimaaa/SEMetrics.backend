ee98293ece968ae930b99fca2830d0e6139d17fd
woodley <gelinw3@gmail.com>
2023-08-29 19:18:33 -0400

    set same site to strict

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
       httpOnly: false,
-      sameSite: "none",
+      sameSite: "strict",
       domain: ".gitpulse.vercel.app",
       secure: true
     },
