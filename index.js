828e436c1d6a19afd41ff0c421b19d579bc7377e
woodley <gelinw3@gmail.com>
2023-08-29 16:42:43 -0400

    try 16

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(
     cookie: {
       domain:'.vercel.app',
       maxAge: 7 * 24 * 60 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
     },
